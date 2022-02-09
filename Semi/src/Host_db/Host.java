91b4bbd5db9e49a66cae3767cd0cf8b89fa55248
yjklp <yjklp@DESKTOP-49BGTOO>
2022-02-09 14:28:23 +0900

    host-DAO

---
+++ Semi/src/Host_db/Host.java
@@ -0,0 +1,101 @@
+package Host_db;
+
+public class Host {
+	private String host_id	 ;
+	private String host_password;
+	private String host_name;
+	private String host_birth;
+	private String addrzipnum;
+	private String addrbasic;
+	private String addrdetail;
+
+	private String host_business;
+	private String host_email;
+	private String business_name;
+	private String business_num;
+	private String corporation_name;
+	private String corporation_num;
+	
+	public String getHost_id() {
+		return host_id;
+	}
+	public void setHost_id(String host_id) {
+		this.host_id = host_id;
+	}
+	public String getHost_password() {
+		return host_password;
+	}
+	public void setHost_password(String host_password) {
+		this.host_password = host_password;
+	}
+	public String getHost_name() {
+		return host_name;
+	}
+	public void setHost_name(String host_name) {
+		this.host_name = host_name;
+	}
+	public String getHost_birth() {
+		return host_birth;
+	}
+	public void setHost_birth(String host_birth) {
+		this.host_birth = host_birth;
+	}
+	public String getAddrzipnum() {
+		return addrzipnum;
+	}
+	public void setAddrzipnum(String addrzipnum) {
+		this.addrzipnum = addrzipnum;
+	}
+	public String getAddrbasic() {
+		return addrbasic;
+	}
+	public void setAddrbasic(String addrbasic) {
+		this.addrbasic = addrbasic;
+	}
+	public String getAddrdetail() {
+		return addrdetail;
+	}
+	public void setAddrdetail(String addrdetail) {
+		this.addrdetail = addrdetail;
+	}
+	public String getHost_business() {
+		return host_business;
+	}
+	public void setHost_business(String host_business) {
+		this.host_business = host_business;
+	}
+	public String getHost_email() {
+		return host_email;
+	}
+	public void setHost_email(String host_email) {
+		this.host_email = host_email;
+	}
+	public String getBusiness_name() {
+		return business_name;
+	}
+	public void setBusiness_name(String business_name) {
+		this.business_name = business_name;
+	}
+	public String getBusiness_num() {
+		return business_num;
+	}
+	public void setBusiness_num(String business_num) {
+		this.business_num = business_num;
+	}
+	public String getCorporation_name() {
+		return corporation_name;
+	}
+	public void setCorporation_name(String corporation_name) {
+		this.corporation_name = corporation_name;
+	}
+	public String getCorporation_num() {
+		return corporation_num;
+	}
+	public void setCorporation_num(String corporation_num) {
+		this.corporation_num = corporation_num;
+	}
+	
+	
+	
+	
+}
---
+++ Semi/src/Host_db/HostDAO.java
@@ -0,0 +1,133 @@
+package Host_db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import Host_db.Host;
+
+
+public class HostDAO {
+private DataSource ds;
+	
+	//생성자에서 JNDI 리소스를 참조하여  Connection 객체를 얻어옵니다.
+	public HostDAO() {
+		try {
+			Context init = new InitialContext();
+			ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+		}catch(Exception ex) {
+			System.out.println("DB 연결 실패: "+ex);
+		}
+		
+	}
+	public int insert(Host h) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int result=0;
+		try {
+			con = ds.getConnection();
+			System.out.println("getConnection : insert()");
+			pstmt = con.prepareStatement( "INSERT INTO HOST "
+	+ "(host_business, host_id, host_password, host_birth,host_name, business_name, business_num, addrzipnum, addrbasic, addrdetail, "
+	+ "host_email, corporation_name, corporation_num) "
+					+ "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+					);
+			
+			pstmt.setString(1, h.getHost_business());
+			pstmt.setString(2, h.getHost_id());
+			pstmt.setString(3, h.getHost_password());
+			pstmt.setString(4, h.getHost_birth());
+			
+			pstmt.setString(5, h.getHost_name());
+			pstmt.setString(6, h.getBusiness_name());
+			pstmt.setString(7, h.getBusiness_num());
+			
+			pstmt.setString(8, h.getAddrzipnum());
+			pstmt.setString(9, h.getAddrbasic());
+			pstmt.setString(10, h.getAddrdetail());
+			pstmt.setString(11, h.getHost_email());
+			pstmt.setString(12, h.getCorporation_name());
+			pstmt.setString(13, h.getCorporation_num());
+
+		
+			result = pstmt.executeUpdate();//삽입 성공시 result 는 1
+			
+		}catch(java.sql.SQLIntegrityConstraintViolationException e) {
+			result = -1;
+			System.out.println("멤버 아이디 중복 에러입니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt !=null)
+				try {
+					pstmt.close();
+				}catch(SQLException ex) {
+					ex.printStackTrace();
+				}
+			if(con !=null)
+				try {
+					con.close();
+				}catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+		}
+		return result;
+	}
+	public Host member_info(String id) {
+		Host h =null;
+		Connection con = null;
+		PreparedStatement pstmt=null;
+		ResultSet rs = null;
+		try {
+			con = ds.getConnection();
+			String sql = "select *from host where host_id=? ";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				h = new Host();
+				h.setHost_id(rs.getString(1));
+				h.setHost_password(rs.getString(2));
+				h.setHost_name(rs.getString(3));
+				h.setHost_birth(rs.getString(4));
+				h.setHost_email(rs.getString(5));
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(rs != null)
+				try {
+					rs.close();
+				}catch(SQLException ex	) {
+					ex.printStackTrace();
+				}
+			if(pstmt !=null)
+				try {
+					pstmt.close();
+				}catch(SQLException ex) {
+					ex.printStackTrace();
+				}
+			if(con != null)
+				try {
+					con.close();
+				}catch(SQLException ex) {
+					ex.printStackTrace();
+				}
+			
+		}
+		return h;
+	}
+	public int update(Host m) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+
+}
